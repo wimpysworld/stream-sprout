53575da58e3e3528a479aaa8f6227bd323666388
Martin Wimpress <martin@wimpress.org>
2024-07-22 12:32:18 +0100

    feat: add nix flake

---
+++ package.nix
@@ -0,0 +1,42 @@
+{ lib
+, installShellFiles
+, makeWrapper
+, stdenv
+, ffmpeg-headless
+, procps
+, yq-go
+}:
+let
+  runtimePaths = [
+    ffmpeg-headless
+    procps
+    yq
+  ];
+  versionMatches =
+    builtins.match ''
+      .*
+      readonly[[:blank:]]VERSION="([[:digit:]]+\.[[:digit:]]+\.[[:digit:]]+)"
+      .*
+    '' (builtins.readFile ./stream-sprout);
+in
+stdenv.mkDerivation rec {
+  pname = "stream-sprout";
+  version = builtins.concatStringsSep "" versionMatches;
+  src = lib.cleanSource ./.;
+
+  nativeBuildInputs = [ makeWrapper installShellFiles ];
+
+  installPhase = ''
+    runHook preInstall
+    install -Dm755 -t "$out/bin" stream-sprout
+    runHook postInstall
+  '';
+
+  meta = {
+    description = "Re-stream a video source to multiple destinations such as Twitch, YouTube, and Owncast.";
+    homepage = "https://github.com/wimpys-world/stream-sprout";
+    mainProgram = "stream-sprout";
+    license = lib.licenses.asl20;
+    maintainers = with lib.maintainers; [ flexiondotorg ];
+  };
+}
